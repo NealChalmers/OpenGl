5da513199f6ee058b0444d984b8e6f14db3aa428
NealChalmers <Chalmers111@qq.com>
2018-09-17 23:29:35 +0800

    update

--- Hello_Triangle.cpp
+++ Hello_Triangle.cpp
@@ -21,6 +21,11 @@ float vertices[] = {
 	-0.5f, 0.5f, 0.0f   // 左上角
 };
 
+unsigned int indices[] = {
+	0, 1, 3, // 第一个三角形
+	1, 2, 3  // 第二个三角形
+};
+
 //Shader
 const GLchar* vertexShaderSource = "#version 330 core\n"
 "layout(location = 0) in vec3 aPos;\n"
@@ -62,13 +67,17 @@ int main()
 
 	glViewport(0, 0, 800, 600);
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -110,6 +119,9 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
