82b55b697d4e613e3c981740cff2a3e1bb8535dc
NealChalmers <Chalmers111@qq.com>
2018-10-17 13:49:28 +0800

    update

--- software engineering/Camera_1.cpp
+++ software engineering/Camera_1.cpp
@@ -74,17 +74,13 @@ glm::vec3 cubePositions[] = {
 
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Coordinate", NULL, NULL);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Coordinate", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-		return -1;
+		return 0;
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
